555f6a66f9ad7a7b7176d6d4a3c0f261d67a28fe
Memo Döring <memodoring@users.noreply.github.com>
2019-10-26 13:00:17 -0300

    use hook to display translation after succesful API call

--- src/App.js
+++ src/App.js
@@ -9,13 +9,28 @@ Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 function TranslateTextComponent(){
   const [originalText, setOriginalText] = useState('Escriba aqui para traducir');
+  const [translation, setTranslation] = useState(`La traduccion aparecera aqui`);
+  
   function setChar(event){
     setOriginalText(event.target.value);
   };
+  
+  function generateTranslation(){
+    Predictions.convert({
+      translateText:{
+        source:{
+          text:originalText
+        }
+      }
+    })
+    .then(data=>setTranslation(JSON.stringify(data)))
+    .catch(error=>setTranslation(JSON.stringify(error)));
+  };;
+
   return(<div>
     <input value={originalText} onChange={setChar}></input>
-    <button>Translate</button>
-    <h3>Traduccion aparecera aqui</h3>
+    <button onClick={generateTranslation}>Traducir</button>
+    <h3>{translation}</h3>
   </div>)
 }
 
